32246d250c23e49e7f10339a014862d7aee37070
caoyuan0816 <caoyuan0816@gmail.com>
2018-02-22 22:14:56 -0700

    Update manifest.txt

--- compile.sh
+++ compile.sh
@@ -19,7 +19,7 @@ echo "\033[0;32mCompiling Server"
 echo "\033[0;31m"
 
 
-javac -d . -cp ".:$ROOT_PATH/src:$LIB_PATH/kryo-4.0.1.jar:$LIB_PATH/kryonet-2.22.0-RC1.jar:$LIB_PATH/jfreechart-1.0.13.jar:$LIB_PATH/jcommon-1.0.23.jar:$LIB_PATH/minlog-1.3.0.jar:$LIB_PATH/objenesis-2.1.jar:$LIB_PATH/reflectasm-1.10.1.jar" $ROOT_PATH/src/server/ServerApp.java
+javac -d . -cp ".:$ROOT_PATH/src:$LIB_PATH/kryo-4.0.1.jar:$LIB_PATH/kryonet-2.22.0-RC1.jar:$LIB_PATH/jfreechart-1.0.13.jar:$LIB_PATH/jcommon-1.0.23.jar:$LIB_PATH/minlog-1.3.0.jar:$LIB_PATH/objenesis-2.1.jar:$LIB_PATH/reflectasm-1.10.1-shaded.jar" $ROOT_PATH/src/server/ServerApp.java
 
 jar cfm server.jar manifest_server.txt lib src server network
 rm -rf server 
@@ -29,7 +29,7 @@ echo "\033[0;32mCompiling Client"
 echo "\033[0;31m"
 
 
-javac -d . -cp ".:$ROOT_PATH/src:$LIB_PATH/kryo-4.0.1.jar:$LIB_PATH/kryonet-2.22.0-RC1.jar:$LIB_PATH/jfreechart-1.0.13.jar:$LIB_PATH/jcommon-1.0.23.jar:$LIB_PATH/minlog-1.3.0.jar:$LIB_PATH/objenesis-2.1.jar:$LIB_PATH/reflectasm-1.10.1.jar" $ROOT_PATH/src/client/ClientApp.java
+javac -d . -cp ".:$ROOT_PATH/src:$LIB_PATH/kryo-4.0.1.jar:$LIB_PATH/kryonet-2.22.0-RC1.jar:$LIB_PATH/jfreechart-1.0.13.jar:$LIB_PATH/jcommon-1.0.23.jar:$LIB_PATH/minlog-1.3.0.jar:$LIB_PATH/objenesis-2.1.jar:$LIB_PATH/reflectasm-1.10.1-shaded.jar" $ROOT_PATH/src/client/ClientApp.java
 
 jar cfm client.jar manifest_client.txt lib src client network
 rm -rf client
